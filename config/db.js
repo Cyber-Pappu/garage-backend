13690d52af5700524ca5831218b098d9fd606acb
Cyber-Pappu <melchizedek981@gmail.com>
2026-06-18 14:25:49 +0530

    Update API URLs to Railway backend

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    port: process.env.MYSQLPORT,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 connection.connect((err) => {
